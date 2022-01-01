73304bfbb742ab3911639a740a07fa32282a43cb
norio6199 <norio6199@gmail.com>
2022-01-01 23:21:44 +0900

    setup storybook

--- .storybook/preview.js
+++ .storybook/preview.js
@@ -1,9 +1,28 @@
+import React from 'react';
+import * as NextImage from 'next/image';
+import { addDecorator } from '@storybook/react';
+import { GlobalStyle } from '../src/components/utils';
+
+const OriginalNextImage = NextImage.default;
+
+Object.defineProperty(NextImage, 'default', {
+  configurable: true,
+  value: (props) => <OriginalNextImage {...props} unoptimized />,
+});
+
+addDecorator((story) => (
+  <>
+    <GlobalStyle />
+    {story()}
+  </>
+));
+
 export const parameters = {
-  actions: { argTypesRegex: "^on[A-Z].*" },
+  actions: { argTypesRegex: '^on[A-Z].*' },
   controls: {
     matchers: {
       color: /(background|color)$/i,
       date: /Date$/,
     },
   },
-}
+};
