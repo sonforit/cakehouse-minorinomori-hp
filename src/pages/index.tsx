3aa6acccf4942ba1b8f1a21082aebe14c2b24b98
norio6199 <norio6199@gmail.com>
2022-01-12 23:26:39 +0900

    contentfulのapiを実行

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -2,8 +2,17 @@ import type { NextPage } from 'next';
 import { GlobalHead } from '@/components/utils';
 import { PAGE } from '@/constants/pages';
 import { HomeTemplate } from '@/components/templates';
+import { EntryCollection, createClient } from 'contentful';
+import type { IProductFields } from '@/types/contentful';
+import { CATEGORY_DECORATION_ID } from '@/constants/contentful';
 
-const Home: NextPage = () => {
+type Props = {
+  products: EntryCollection<IProductFields>;
+};
+
+const Home: NextPage<Props> = ({ products }) => {
+  console.log({ products });
+
   return (
     <div id="page-home">
       <GlobalHead title={PAGE.HOME.LABEL} description={PAGE.HOME.DESCRIPTION} />
@@ -13,3 +22,22 @@ const Home: NextPage = () => {
 };
 
 export default Home;
+
+export const getStaticProps = async () => {
+  const client = createClient({
+    space: process.env.CONTENTFUL_SPACE_ID || '',
+    accessToken: process.env.CONTENTFUL_ACCESS_KEY || '',
+  });
+
+  const products = await client.getEntries<IProductFields>({
+    content_type: 'product',
+    'fields.category.sys.id': CATEGORY_DECORATION_ID,
+    limit: 9,
+  });
+
+  return {
+    props: {
+      products,
+    },
+  };
+};
