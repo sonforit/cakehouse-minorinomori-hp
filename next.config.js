357ac0ce0936bd42f3dbb660677f3dff446ebd0a
norio6199 <norio6199@gmail.com>
2022-01-13 23:39:13 +0900

    github pagesへのデプロイ準備

--- next.config.js
+++ next.config.js
@@ -1,5 +1,7 @@
 /** @type {import('next').NextConfig} */
 module.exports = {
+  basePath: process.env.GITHUB_ACTIONS ? '/cakehouse-minorinomori-hp' : '',
+  trailingSlash: true,
   reactStrictMode: true,
   images: {
     domains: ['images.ctfassets.net'],
